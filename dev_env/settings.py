7fc56d5ffdada5b883d6ee5ff361c9fb1b7150c3
Othniel-dell <othnieldavidson9@gmail.com>
2025-02-03 15:49:55 +0000

    everyhost

--- dev_env/settings.py
+++ dev_env/settings.py
@@ -10,7 +10,7 @@ if not SECRET_KEY:
     SECRET_KEY = 'djangoledger1234!DoNotUse!BadIdea!VeryInsecure!'
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "*"]
 CSRF_TRUSTED_ORIGINS = ['https://*.preview.app.github.dev']
 
 INSTALLED_APPS = [
