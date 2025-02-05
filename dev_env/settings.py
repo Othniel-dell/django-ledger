dd93614a90d2f68dffdcc534fdf8fa51eca3a796
Othniel-dell <othnieldavidson9@gmail.com>
2025-02-05 10:37:38 +0000

    db

--- dev_env/settings.py
+++ dev_env/settings.py
@@ -238,13 +238,24 @@ WSGI_APPLICATION = 'dev_env.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'great_soanbe_mon_db',
+        'USER': 'great_soanbe_mon_db_user',
+        'PASSWORD': 'cuhmUSSyAt25oDnD7FRHQWonpxHdk6Q8',
+        'HOST': 'dpg-cuhjoul2ng1s7387r5pg-a.oregon-postgres.render.com',
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
